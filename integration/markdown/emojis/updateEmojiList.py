c5e1adfa5a3ede64df73e06ae4649a7ed0cdacfc
Fenton <fentonreid@gmail.com>
2021-03-23 15:07:14 +0000

    Update updateEmojiList.py

--- integration/markdown/emojis/updateEmojiList.py
+++ integration/markdown/emojis/updateEmojiList.py
@@ -1,22 +1,47 @@
 import os
 import sys
 
+from requests import get
+
 sys.path.append("\\".join(os.getcwd().split("\\")[:-3]))
 
 from integration.markdown.emojis.shortCodeDict import getCommonEmojis
 # code to automatically upload the emojiList found in shortCodeDict.py
 
-newEmojiList = getCommonEmojis()[1]
+# PRINT THE FOLLOWING CODE FROM GITHUB
+emoji_names = get("https://raw.githubusercontent.com/zulip/zulip/master/tools/setup/emoji/emoji_names.py")
+print(emoji_names.content.decode())
+# get the user input
+print("\nThe following code from, https://raw.githubusercontent.com/zulip/zulip/master/tools/setup/emoji/emoji_names.py has been printed above. Does this seem safe?\n")
+userConsent = str(input("Do you want to proceed (y/n): "))
 
-if str(type(newEmojiList)).find("list"):
-    # get current contents of shortCodeDict.py
-    with open('shortCodeDict.py', 'r', encoding="utf8") as scd:
-        contents = scd.readlines()
+if userConsent in ['y', 'Y', 'yes', 'YES']:
+    print("UPDATING")
+    tupleResult = getCommonEmojis()
 
-    with open('shortCodeDict.py', 'w', encoding="utf8") as scd:
-        for count, line in enumerate(contents):
-            if line.find("return [") > -1:
-                contents[count] = "    return " + str(newEmojiList) + "\n"
-                break
+    if str(type(tupleResult[1])).find("list"):
+        # get current contents of shortCodeDict.py
+        with open('shortCodeDict.py', 'r', encoding="utf8") as scd:
+            contents = scd.readlines()
 
-        scd.writelines(contents)
+        # update emojiList
+        with open('shortCodeDict.py', 'w', encoding="utf8") as scd:
+            for count, line in enumerate(contents):
+                if line.find("return [") > -1:
+                    contents[count] = "    return " + str(tupleResult[1]) + "\n"
+                    break
+
+        # update Slack To Zulip Dict
+        with open('shortCodeDict.py', 'w', encoding="utf8") as scd:
+            counter = 0
+            for count, line in enumerate(contents):
+                if line.find("return {") > -1:
+                    counter += 1
+
+                if counter == 3:
+                    contents[count] = "    return " + str(tupleResult[0]) + "\n"
+                    break
+
+            scd.writelines(contents)
+else:
+    print("STOPPING WITHOUT EXECUTION")
