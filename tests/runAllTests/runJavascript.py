37bac325a7de4f8a002d5b419ce1f6217aea98ad
Fenton <fentonreid@gmail.com>
2021-03-23 10:51:24 +0000

    Update runJavascript.py

--- tests/runAllTests/runJavascript.py
+++ tests/runAllTests/runJavascript.py
@@ -1,4 +1,9 @@
 import os
+import sys
+
 # create the jasmine server instance, automatically detects and runs the spec.js test
 os.chdir("../javascript")
-os.system("jasmine server -p 5000")
+try:
+    os.system(f"jasmine server -p {sys.argv[1]}")
+except:
+    os.system("jasmine server -p 5000")
